accf41dd06e4f699f07c15a16c8fafb9b6ee7faf
Colin Johnson <blisk76@gmail.com>
2020-05-03 14:43:25 -0400

    Setup Routes

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,41 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//Routes
+//===============================
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../notes.html"));
+});
+
+// If no matching route is found default to home
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+
+//API Routes
+//===============================
+
+app.get("/api/notes", function (req, res) {
+    return res.json(characters);
+  });
+  
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT:${PORT}`);
+});
