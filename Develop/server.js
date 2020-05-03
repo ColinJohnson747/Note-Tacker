16e1ebce0313668f2eb52ac585a3541760f97384
Colin Johnson <blisk76@gmail.com>
2020-05-03 16:36:11 -0400

    made one main file, issues having it split

--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,62 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const port = 3000 || process.env.PORT;
+const mainDir = path.join(__dirname, "/public");
 
-// Sets up the Express app to handle data parsing
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(mainDir, "notes.html"));
+});
 
-app.listen(PORT, () => {
+app.get("/api/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes/:id", function (req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(savedNotes[Number(req.params.id)]);
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(mainDir, "index.html"));
+});
+
+app.post("/api/notes", function (req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let newNote = req.body;
+  let uniqueID = savedNotes.length.toString();
+  newNote.id = uniqueID;
+  savedNotes.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+  console.log("Note saved to db.json. Content: ", newNote);
+  res.json(savedNotes);
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteID = req.params.id;
+  let newID = 0;
+  console.log(`Deleting note with ID ${noteID}`);
+  savedNotes = savedNotes.filter((currNote) => {
+    return currNote.id != noteID;
+  });
+
+  for (currNote of savedNotes) {
+    currNote.id = newID.toString();
+    newID++;
+  }
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+  res.json(savedNotes);
+});
+
+app.listen(port, function () {
+  console.log(`Now listening to port ${port}.`);
 });
